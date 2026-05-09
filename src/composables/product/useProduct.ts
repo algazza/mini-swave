b2481fcc58a79c642b6f92f677c84f416fdb8bce
Al Ghaza <fadhilaswadira72564@gmail.com>
2026-05-10 01:27:21 +0700

    🎨 perf: improve structure code

--- src/composables/product/useProduct.ts
+++ src/composables/product/useProduct.ts
@@ -2,14 +2,15 @@ import { createQuery } from "@tanstack/svelte-query";
 import type { AxiosError } from "axios";
 import type { ErrorResponse } from "../../types/errors";
 import { api } from "../../service/api";
+import type { ProductType } from "../../types/product";
 
 export const useProduct = () => {
-  return createQuery(() => ({
+  return createQuery<ProductType[], Error>(() => ({
     queryKey: ["products"],
     queryFn: async () => {
       try {
         const res = await api.get("/get_all_product");
-        return res.data;
+        return res.data.data as ProductType[];
       } catch (err) {
         const error = err as AxiosError<ErrorResponse>;
         throw new Error(
